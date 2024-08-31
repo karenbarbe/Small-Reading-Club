356b682b4485090baa7bb25991259ec8b1253bf3
info@karenbarbe.com <177651097+karenbarbe@users.noreply.github.com>
2024-08-31 11:45:48 -0500

    move database to separate file
    - create new file database.js
    - remove database from script.js
    - add new script tag to index.html

---
+++ database.js
@@ -0,0 +1,66 @@
+// Books database
+const books = [
+  {
+    country: 'Mexico',
+    path: 'lilianasinvinciblesummer',
+    images: ['cover', '1', '2', '3', '4'],
+    title: 'Liliana’s Invincible Summer',
+    author: 'Cristina Rivera Garza',
+    genre: ['Memoir', 'Nonfiction', 'Biography', 'Feminism', 'Latin American'],
+    year: 2021,
+    summary: 'In this gut-wrenching blend of memoir and reportage, Rivera Garza  investigates her younger sister Liliana’s 1990 murder by an abusive ex-boyfriend, who remains at large. Placing her sister’s death in the context of the femicide crisis in Mexico, Rivera Garza documents and recreates the years and months leading up to her murder, interweaving startling facts, case files and newspaper accounts with lyrical meditations on her family life and Liliana’s efforts to break away from her obsessive high school boyfriend. The excerpts from Liliana’s teenage diary and her oft-repeated desire not to be left alone haunt the narrative, as do Rivera Garza’s guilt and shame over her sister’s death.',
+    awards: ['Pulitzer Prize for Memoir or Autobiography (2024)', 'Premio Xavier Villaurrutia for Novela (2021)', 'National Book Award Finalist for Non-Fiction (2023)'],
+    recommendation: 'This book gives us the insight and the language to identify the signs of danger and to see in plain sight how patriarchy deforms and hurts men, as much as it does women.'
+  },
+
+  {
+    country: 'United States',
+    path: 'fierceattachments',
+    images: ['cover', '1', '2', '3', '4'],
+    title: 'Fierce Attachments',
+    author: 'Vivian Gornick',
+    genre: ['Memoir', 'Nonfiction', 'Biography', 'Feminism'],
+    year: 1987,
+    summary: 'In this deeply etched and haunting memoir, Vivian Gornick tells the story of her lifelong battle with her mother for independence. Born and raised in the Bronx, Gornick grows up in a household dominated by her intelligent but uneducated mother’s romantic depression over the early death of her husband. Next door lives Nettie, an attractive widow whose calculating sensuality appeals greatly to Vivian. These women with their opposing models of femininity continue, well into adulthood, to affect Gornick’s struggle to find herself in love and in work.',
+    awards: ['Premio de los Libreros de Madrid for Fiction (2017)', 'New York Times #1 Best Memoir of the Past 50 Years (2019)'],
+    recommendation: 'One of the most remarkable documents of family feeling that has been written, a classic that helped start the memoir boom and remains one of the most moving examples of the genre.'
+  },
+
+  {
+    country: 'United States',
+    path: 'gorillasinmist',
+    images: ['cover', '1', '2', '3', '4', '5'],
+    title: 'Gorillas in the Mist',
+    author: 'Dian Fossey',
+    genre: ['Memoir','Nonfiction', 'Animals', 'Nature', 'Science'],
+    year: 1983,
+    summary: 'One of the most important books ever written about our connection to the natural world, Gorillas in the Mist is the riveting account of Dian Fossey’s thirteen years in a remote African rain forest with the greatest of the great apes. Fossey’s extraordinary efforts to ensure the future of the rain forest and its remaining mountain gorillas are captured in her own words and in candid photographs of this fascinating endangered species.',
+    awards: ['Inspiration for the Academy Award–nominated film Gorillas in the Mist (1988)'],
+    recommendation: 'Although Fossey’s work ended tragically with her murder, Gorillas in the Mist remains an invaluable testament to one of the longest-running field studies of primates and reveals her undying passion for her subject.'
+  },
+
+  {
+    country: 'Germany',
+    path: 'hiddenlifetrees',
+    images: ['cover'],
+    title: 'The Hidden Life of Trees',
+    author: 'Peter Wohlleben',
+    genre: ['Environmental literature', 'Nature', 'Forestry', 'Non-Fiction'],
+    year: 2015,
+    summary: 'Did you know that trees can communicate with each other and have their own way to nurture each other? If not then this book is for you. Peter Wohlleben describes in his book how trees have a network of communication with each other and how they balance the ecosystem of the woods. This book changes your idea of the woods and their impact on our global ecosystem.',
+    awards: ['Australian Book Industry Awards (2017)', 'American Booksellers Association Indies Choice Book Award for Nonfiction (2017)', 'Rob Stewart Eco Warrior Award (2020)'],
+    recommendation: 'The book changed my view on trees and how important they are to humankind. Peter Wohlleben is a German forest ranger who does not only write from his own experiences but uses also sources. Also if this book is non-fiction, it is very interesting to read.'
+  },
+  {
+    country: 'Argentina',
+    path: 'ourshareofnight',
+    images: ['cover', '1', '2', '3', '4', '5', '6'],
+    title: 'Our Share of Night',
+    author: 'Mariana Enriquez',
+    genre: ['Horror', 'Fiction', 'Historical Fiction', 'Latin American', 'Queer'],
+    year: 2019,
+    summary: 'Moving back and forth in time, during the brutal years of Argentina’s military dictatorship and its turbulent aftermath, Our Share of Night is a novel like no other: a family story, a ghost story, a story of the occult and the supernatural, a book about the complexities of love and longing with queer subplots and themes.',
+    awards: ['Premio Herralde de Novela (2019)', 'Tähtifantasia Award Nominee (2023)', 'Premio de la Crítica de Narrativa Castellana (2019)', 'The Kitschies Nominee for Red Tentacle (Novel) (2023)', 'Premio Celsius (2020)', 'Goodreads Choice Award Nominee for Horror (2023)'],
+    recommendation: 'tbd'
+  }
+];
